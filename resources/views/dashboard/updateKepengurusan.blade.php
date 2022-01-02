0bf2283c248193c93be357220d04714b500cbf6c
virayulia <virayukia1234@gmail.com>
2022-01-02 09:13:17 +0700

    edit kepengurusan

--- resources/views/dashboard/updateKepengurusan.blade.php
+++ resources/views/dashboard/updateKepengurusan.blade.php
@@ -25,12 +25,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Tambah Kepengurusan</h1>
+            <h1 class="m-0">Edit Kepengurusan</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Tambah Kepengurusan</li>
+              <li class="breadcrumb-item active">Edit Kepengurusan</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
